ae947c6d88389a9efeb2a76962ad341d52c43947
wobbler <tom.miklik@gmail.com>
2021-10-24 14:57:43 +0200

    Final version 1.0

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
      url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
      url(r'^post/new/$', views.post_new, name='post_new'),
      url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+     path('drafts/', views.post_draft_list, name='post_draft_list'),
+     path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+     path('post/<pk>/remove/', views.post_remove, name='post_remove'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -18,8 +18,7 @@ def post_new(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
+            post.author = request.user            
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -32,10 +31,23 @@ def post_edit(request, pk):
          form = PostForm(request.POST, instance=post)
          if form.is_valid():
              post = form.save(commit=False)
-             post.author = request.user
-             post.published_date = timezone.now()
+             post.author = request.user             
              post.save()
              return redirect('post_detail', pk=post.pk)
      else:
          form = PostForm(instance=post)
      return render(request, 'post_edit.html', {'form': form})
+    
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
